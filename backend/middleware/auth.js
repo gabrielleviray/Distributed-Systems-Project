d45f7598b60032dd2d145a93081bc548b63b5cad
Amy Phan <amyyyyphan99@gmail.com>
2022-11-10 00:31:23 -0800

    added token verification middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const User = require('../models/User');
+
+exports.verifyToken = async (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+  } else {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+};
