19357ee09fc84da89a522d3da1b68df528eeb066
gabrielleviray <gabrielle.viray@sjsu.edu>
2022-11-30 19:37:27 -0800

    converted DateTime to readable date and time

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -7,7 +7,14 @@ const RecipeDetails = ({ recipe }) => {
     //     const response = await fetch('/api/recipe/all')
     // }
 
-    
+    let DateTime = recipe.createdAt
+    let timestamp = new Date(DateTime).getTime().toString();
+    let Day = new Date(DateTime).getDate();
+    let Month = new Date(DateTime).toLocaleString('default', {Month: 'short'});
+    let Year = new Date(DateTime).getFullYear();
+    // let convertedDateTime = `${Month}/${Day}/${Year}`;
+    let convertedDateTime = `${Month}`;
+
     return(
 
         <div className = "card home-card" >
@@ -15,7 +22,7 @@ const RecipeDetails = ({ recipe }) => {
             <p><strong> Time: </strong> {recipe.time} </p>
             <p><strong> Ingredients: </strong> {recipe.ingredients} </p>
             <p><strong> Directions: </strong> {recipe.directions} </p>
-            <Link to={"/user/"+recipe.username}><p align="right">posted by {recipe.author} on {recipe.createdAt} </p></Link>
+            <Link to={"/user/"+recipe.username}><p align="right">posted by {recipe.author} on {Month}</p></Link>
             {/* <span onClick={onSubmit}>delete</span> */}
         </div>
     )
